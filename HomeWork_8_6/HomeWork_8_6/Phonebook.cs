607d01dc1d42da50b4fd07f2fc3e4850c40a113f
Marlen Zhumakov <m_zhumakov@hotmail.com>
2023-04-13 23:49:56 +0600

    added task 2 solution

---
+++ HomeWork_8_6/HomeWork_8_6/Phonebook.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HomeWork_8_6
+{
+    internal class Phonebook
+    {
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Метод итеративно предлагает пользователю вводить в программу номера телефонов и ФИО их владельцев.
+        /// Признаком того, что пользователь закончил вводить номера телефонов, является ввод пустой строки. 
+        /// </summary>
+        public void InputPhoneNumbers()
+        {
+            string phoneNumber = " ";
+            string fullName = " ";
+
+            while (true)
+            {
+                Console.WriteLine("-------------------------------------------------------------------------------------------");
+                Console.WriteLine("Введите номер телефона (если вы закончили вводить номера телефонов, просто нажмите Enter): ");
+                phoneNumber = Console.ReadLine();
+
+                if(phoneNumber.Equals(""))
+                {
+                    break;
+                }          
+
+                Console.WriteLine("Введите ФИО владельца: ");
+                fullName = Console.ReadLine();
+
+                if (fullName.Equals(""))
+                {
+                    break;
+                }
+
+                Console.WriteLine("-------------------------------------------------------------------------------------------");
+
+                if(!phonebook.ContainsKey(phoneNumber))
+                {
+                    phonebook.Add(phoneNumber, fullName);
+                }
+                else
+                {
+                    Console.WriteLine("Такой номер уже существует, пожалуйста, введите другой номер!");
+                    Console.WriteLine();
+                    Console.WriteLine("Нажмите Enter, что бы попробовать снова...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Пользователь вводит номер телефона и ему выдаётся ФИО владельца, после чего приложение закрывается. 
+        /// </summary>
+        public void OutputPhoneNumbersOwner()
+        {
+            string phoneNumber = " ";
+            string value = "";
+            bool correctNumber = false;            
+
+            while(!correctNumber)
+            {
+                if (phonebook.Count == 0)
+                {
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    Console.WriteLine($"Записная книжка пуста! Нужно перезапустить приложение!");
+                    Console.WriteLine();
+                    Console.WriteLine("Нажмите Enter, что бы выйти...");
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    Console.ReadKey();
+                    break;
+                }
+
+                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                Console.WriteLine("Введите номер телефона, что бы найти его владельца: ");
+                phoneNumber = Console.ReadLine();                
+
+                if (phonebook.TryGetValue(phoneNumber, out value))
+                {
+                    Console.WriteLine();
+                    string name = phonebook[phoneNumber];
+                    correctNumber = true;
+                    Console.WriteLine();
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    Console.WriteLine($"Владельцем номера {phoneNumber} является {name}");
+                    Console.WriteLine();
+                    Console.WriteLine("Нажмите Enter, что бы выйти...");
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    correctNumber = false;
+                    Console.WriteLine($"Номер {phoneNumber} остутсвует в телефонной книге! Проверьте правильность введенного номера!");
+                    Console.WriteLine();
+                    Console.WriteLine("Нажмите Enter, что бы попробовать снова...");
+                    Console.ReadKey();
+                }
+            }            
+        }
+    }
+}
--- HomeWork_8_6/HomeWork_8_6/Program.cs
+++ HomeWork_8_6/HomeWork_8_6/Program.cs
@@ -10,9 +10,20 @@ namespace HomeWork_8_6
     {
         static void Main(string[] args)
         {
-            /// Задание 1. Работа с листом
+            // Задание 1. Работа с листом        
+            //Task1List();
+
+            // Задание 2. Телефонная книга
+            Task2Phonebook();
+        }
+
+        /// <summary>
+        /// Задание 1. Работа с листом
+        /// </summary>
+        public static void Task1List()
+        {
             Console.WriteLine("Задание 1. Работа с листом");
-            
+
             WorkWithList workWithList = new WorkWithList();
 
             workWithList.FillTheList();
@@ -22,6 +33,35 @@ namespace HomeWork_8_6
             workWithList.PrintList("После удаления чисел > 25 и < 50");
 
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Задание 2. Телефонная книга
+        /// </summary>
+        public static void Task2Phonebook()
+        {
+            Console.WriteLine("Задание 2. Телефонная книга");
+
+            Phonebook phonebook = new Phonebook();
+
+            phonebook.InputPhoneNumbers();
+            phonebook.OutputPhoneNumbersOwner();
+        }
+
+        /// <summary>
+        /// Задание 3. Проверка повторов
+        /// </summary>
+        public static void Task3Duplicates()
+        {
+            
+        }
+
+        /// <summary>
+        /// Задание 4. Записная книжка
+        /// </summary>
+        public static void Task4Jotter()
+        {
+
         }
     }
 }
