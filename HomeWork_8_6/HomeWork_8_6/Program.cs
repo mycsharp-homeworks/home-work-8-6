405ed4f00a4bc22e3881d6bc458fbc3346780c2a
Marlen Zhumakov <m_zhumakov@hotmail.com>
2023-04-13 15:52:14 +0600

    added task 1

--- HomeWork_8_6/HomeWork_8_6/Program.cs
+++ HomeWork_8_6/HomeWork_8_6/Program.cs
@@ -10,6 +10,18 @@ namespace HomeWork_8_6
     {
         static void Main(string[] args)
         {
+            /// Задание 1. Работа с листом
+            Console.WriteLine("Задание 1. Работа с листом");
+            
+            WorkWithList workWithList = new WorkWithList();
+
+            workWithList.FillTheList();
+            workWithList.PrintList("Полный лист");
+
+            workWithList.DeleteNumbers();
+            workWithList.PrintList("После удаления чисел > 25 и < 50");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ HomeWork_8_6/HomeWork_8_6/WorkWithList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_8_6
+{
+    class WorkWithList
+    {
+        public List<int> ints = new List<int>();
+
+        /// <summary>
+        /// Метод заполняет лист 100 случайными числами в диапазоне от 0 до 100.
+        /// </summary>
+        public void FillTheList()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                int randomNumber = rand.Next(0, 101);
+                ints.Add(randomNumber);
+            }
+        }
+
+        /// <summary>
+        /// Метод удаляет из листа числа, которые больше 25, но меньше 50. 
+        /// </summary>
+        public void DeleteNumbers()
+        {
+            int num1 = 25;
+            int num2 = 50;
+
+            // Считываем лист задом наперед. Если идти вперед, то можно упустить некоторые числа,
+            // так как при удалении элементов, список сдвигается влево, меняя индекс следующего числа и может перепрыгнуть через нужное число.
+            for (int i = ints.Count - 1; i >= 0; i--)
+            {
+                if(ints[i] > num1 && ints[i] < num2)
+                {
+                    ints.RemoveAt(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод выводит числа из листа на экран. 
+        /// </summary>
+        public void PrintList(string message)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(message + ":");
+            foreach (var number in ints)
+            {
+                Console.Write(number + " ");
+            }                       
+        }
+    }
+}
